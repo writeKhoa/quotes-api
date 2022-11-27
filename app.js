6b571a98b78baf552565224baeaa16e2bd8e98ed
writeKhoa <khoawrite@gmail.com>
2022-11-27 18:09:41 +0700

    fix morgan

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
+require("dotenv").config("");
 const express = require("express");
-const morgan = require("morgan");
 const app = express();
 
+if (process.env.MODE === "development") {
+  const morgan = require("morgan");
+  app.use(morgan("dev"));
+}
 
-app.use(morgan("dev"));
 app.use(express.json());
 
 require("./src/v1/helpers/connect_mongodb.js");
